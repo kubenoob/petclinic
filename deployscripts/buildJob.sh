6c6369f70ed3b286433980b2499d0294e0c38e30
Kiran <kiranops@yahoo.com>
2021-10-28 08:24:38 +0530

    updated agent pool and environment

--- deployscripts/buildJob.sh
+++ deployscripts/buildJob.sh
@@ -2,10 +2,6 @@
 
 # This script runs build job and saves the artifact to local folder.
 
-
-sudo apt-get update 
-sudo apt-get install maven -y
-
 echo $buildNumber
 echo $buildDir
 artifactsPath="/home/azadmin/agent/artifacts"
@@ -16,5 +12,5 @@ mvn package
 
 # Save the file to the Artifacts here   
 echo "Saving Artifact to Artifcats folder"   
-cp $buildDir/s/target/petclinic.war $artifactsPath/petclinic_$buildNumber.war
+cp $buildDir/s/target/petclinic.war $artifactsPath/petclinic_$buildId.war
 
