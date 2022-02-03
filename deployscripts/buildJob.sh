cf02c0d507936fd7b0f9422156a36d62c865fab5
kiran <kiranops@yahoo.com>
2022-02-03 23:12:27 +0530

    running commands in privileged mode

--- deployscripts/buildJob.sh
+++ deployscripts/buildJob.sh
@@ -6,12 +6,15 @@ echo $buildNumber
 echo $buildDir
 artifactsPath="/home/ubuntu/artifacts"
 
-mkdir -p $artifactsPath
+sudo mkdir -p $artifactsPath
+sudo chown -R $artifactsPath
+sudo chmod u+s -R $artifactsPath
+sudo chmod g+s -R $artifactsPath
 
 # Compiling the Appliction with Maven.
 mvn package     
 
 # Save the file to the Artifacts here   
 echo "Saving Artifact to Artifcats folder"   
-cp $buildDir/s/target/petclinic.war $artifactsPath/petclinic_$buildId.war
+sudo cp $buildDir/s/target/petclinic.war $artifactsPath/petclinic_$buildId.war
 
